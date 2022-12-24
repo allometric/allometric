a5124ea73302fb70ac3b44ae9f37ed08e7ed3176
brycefrank <bfrank70@gmail.com>
2022-12-24 10:20:15 -0800

    adding variable_defs

---
+++ R/variable_defs.R
@@ -0,0 +1,93 @@
+d_defs <- read.csv(system.file('variable_defs/d.csv', package='allometric'))
+v_defs <- read.csv(system.file('variable_defs/v.csv', package='allometric'))
+h_defs <- read.csv(system.file('variable_defs/h.csv', package='allometric'))
+b_defs <- read.csv(system.file('variable_defs/b.csv', package='allometric'))
+component_defs <- read.csv(system.file('variable_defs/components.csv', package='allometric'))
+measure_defs <- read.csv(system.file('variable_defs/measures.csv', package='allometric'))
+
+# TODO a lot of repetition in these functions, is there a safe way to simplify?
+
+get_vol_def <- function(response_name) {
+    measure_char <- substr(response_name, 1, 1)
+    component_char <- substr(response_name, 2, 2)
+    stem_char <- substr(response_name, 3, 3)
+
+
+    matching_measure <- v_defs[
+        v_defs$measure == measure_char &
+        v_defs$component == component_char &
+        v_defs$stem_modifier == stem_char,
+    ]
+
+    add_component <- merge(matching_measure, component_defs)
+    add_measure <- merge(add_component, measure_defs)
+
+    add_measure
+}
+
+get_biomass_def <- function(response_name) {
+    measure_char <- substr(response_name, 1, 1)
+    component_char <- substr(response_name, 2, 2)
+
+    matching_measure <- b_defs[
+        b_defs$measure == measure_char &
+        b_defs$component == component_char,
+    ]
+
+    add_component <- merge(matching_measure, component_defs)
+    add_measure <- merge(add_component, measure_defs)
+    add_measure
+}
+
+get_height_def <- function(response_name) {
+    measure_char <- substr(response_name, 1, 1)
+    component_char <- substr(response_name, 2, 2)
+    height_char <- substr(response_name, 3, 3)
+
+    matching_measure <- h_defs[
+        h_defs$measure == measure_char &
+        h_defs$component == component_char &
+        h_defs$height_modifier == height_char,
+    ]
+
+    add_component <- merge(matching_measure, component_defs)
+    add_measure <- merge(add_component, measure_defs)
+    add_measure
+}
+
+get_diameter_def <- function(response_name) {
+    measure_char <- substr(response_name, 1, 1)
+    component_char <- substr(response_name, 2, 2)
+    bark_char <- substr(response_name, 3, 3)
+    height_char <- substr(response_name, 4, 4)
+
+
+    matching_measure <- d_defs[
+        d_defs$measure == measure_char &
+        d_defs$component == component_char &
+        d_defs$bark_modifier == bark_char &
+        d_defs$height_modifier == height_char,
+    ]
+
+    add_component <- merge(matching_measure, component_defs)
+    add_measure <- merge(add_component, measure_defs)
+
+    add_measure
+}
+
+
+get_variable_def <- function(response_name) {
+    measure_char <- substr(response_name, 1, 1)
+
+    if(measure_char == 'v') {
+        def <- get_vol_def(response_name)
+    } else if(measure_char == 'b') {
+        def <- get_biomass_def(response_name)
+    } else if(measure_char == 'h') {
+        def <- get_height_def(response_name)
+    } else if(measure_char == 'd') {
+        def <- get_diameter_def(response_name)
+    }
+
+    def
+}
--- build_scripts/update_local_models.R
+++ build_scripts/update_local_models.R
@@ -5,62 +5,78 @@ devtools::load_all(".")
 pub_r_files <- list.files("./inst/publications")
 pub_r_paths <- file.path("./inst/publications", pub_r_files)
 
-mod_rows <- list()
-for (k in seq_along(pub_r_paths)) {
-  pub_r_path <- pub_r_paths[[k]]
-  pub_r_file <- pub_r_files[[k]]
+model_data <- list()
+
+for(i in seq_along(pub_r_paths)) {
+  pub_r_path <- pub_r_paths[[i]]
+  pub_r_file <- pub_r_files[[i]]
   print(paste("Processing file:", pub_r_path))
   source(pub_r_path)
   pub_name <- tools::file_path_sans_ext(pub_r_file)
 
   pub <- eval(str2expression(pub_name))
 
-  pub@model_sets
-  for (i in 1:length(pub@model_sets)) {
-    model_response <- pub@model_sets[[i]]
-    for (j in 1:length(model_response)) {
-      model_set <- model_response[[j]]
-      for(l in 1:length(model_set@models)) {
-        mod <- model_set@models[[l]]
-
-        mod_rows[[length(mod_rows) + 1]] <- tibble(
-          pub_id = pub@id,
-          component = get_component_label(mod),
-          measure = get_measure_label(mod),
-          family = mod@model_description$family,
-          genus = mod@model_description$genus,
-          country = mod@model_description$country,
-          region = mod@model_description$region,
-          species = mod@model_description$species,
-          mod = c(mod)
-        )
-      }
-    }
-  }
+  model_data[[pub@id]] <- pub
 }
 
-model_data <- bind_rows(mod_rows)
+saveRDS(model_data, file='./inst/model_data.RDS')
 
-# TODO make this data structure instead: (using json just to show...)
-
-#{
-#  'brackett_1977': {
-#    id: 'brackett_1977',
-#    ...
-#    model_sets: {
-#      'hsa': [
-#        model_1
-#      ],
-#      'vsa': [
-#        model_2
-#      ]
-#    }
-#  },
-#  'temesgen_2008': {
+#mod_rows <- list()
+#for (k in seq_along(pub_r_paths)) {
+#  pub_r_path <- pub_r_paths[[k]]
+#  pub_r_file <- pub_r_files[[k]]
+#  print(paste("Processing file:", pub_r_path))
+#  source(pub_r_path)
+#  pub_name <- tools::file_path_sans_ext(pub_r_file)
 #
+#  pub <- eval(str2expression(pub_name))
+#
+#  pub@model_sets
+#  for (i in 1:length(pub@model_sets)) {
+#    model_response <- pub@model_sets[[i]]
+#    for (j in 1:length(model_response)) {
+#      model_set <- model_response[[j]]
+#      for(l in 1:length(model_set@models)) {
+#        mod <- model_set@models[[l]]
+#
+#        mod_rows[[length(mod_rows) + 1]] <- tibble(
+#          pub_id = pub@id,
+#          component = get_component_label(mod),
+#          measure = get_measure_label(mod),
+#          family = mod@model_description$family,
+#          genus = mod@model_description$genus,
+#          country = mod@model_description$country,
+#          region = mod@model_description$region,
+#          species = mod@model_description$species,
+#          mod = c(mod)
+#        )
+#      }
+#    }
 #  }
 #}
-
-# Then from this you can make a flat-file for searching, but it will be much
-# easier to make the documentation files using this structure, since all the
-# information is available!
+#
+#model_data <- bind_rows(mod_rows)
+#
+## TODO make this data structure instead: (using json just to show...)
+#
+##{
+##  'brackett_1977': {
+##    id: 'brackett_1977',
+##    ...
+##    model_sets: {
+##      'hsa': [
+##        model_1
+##      ],
+##      'vsa': [
+##        model_2
+##      ]
+##    }
+##  },
+##  'temesgen_2008': {
+##
+##  }
+##}
+#
+## Then from this you can make a flat-file for searching, but it will be much
+## easier to make the documentation files using this structure, since all the
+## information is available!
