9fcfa51f1bf64b7a65ef33bcf47b719a4f3be1db
brycefrank <bfrank70@gmail.com>
2022-12-28 05:31:41 -0800

    adding moore_1996 file

---
+++ inst/publications/moore_1996.R
@@ -0,0 +1,49 @@
+library(units)
+
+moore_1996 <- Publication(
+    citation = RefManageR::BibEntry(
+        bibtype = "article",
+        title = "Height-diameter equations for ten tree species in the Inland Northwest",
+        author = "Moore, James A and Zhang, Lianjun and Stuck, Dean",
+        journal = "Western Journal of Applied Forestry",
+        volume=11,
+        number=4,
+        pages="132--137",
+        year=1996,
+        publisher="Oxford University Press"
+    ),
+    descriptors = list(
+        country = "US",
+        region = c("US-MT", "US-OR", "US-WA", "US-ID")
+    )
+)
+
+wykoff_set <- ModelSet(
+    response_unit = list(
+        hst = as_units('ft')
+    ),
+    covariate_units = list(
+        dsob = as_units('in')
+    ),
+    predict_fn = function(dsob) {
+        4.5 + exp(a + (b / (dsob + 1)))
+    },
+    model_specifications = load_parameter_frame('hst_moore_1996_1')
+)
+
+lundqvist_set <- ModelSet(
+    response_unit = list(
+        hst = as_units('ft')
+    ),
+    covariate_units = list(
+        dsob = as_units('in')
+    ),
+    predict_fn = function(dsob) {
+        4.5 + a * exp(-b * dsob^(-c))
+    },
+    model_specifications = load_parameter_frame('hst_moore_1996_2')
+)
+
+moore_1996 <- moore_1996 %>%
+    add_set(wykoff_set) %>%
+    add_set(lundqvist_set)
