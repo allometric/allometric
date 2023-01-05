c46ea87d50c57eda4cca63bf6ab8dd18e566485d
brycefrank <bfrank70@gmail.com>
2023-01-05 07:22:35 -0800

    exposing publications to user with load_publication, closes #26

--- R/load.R
+++ R/load.R
@@ -6,3 +6,14 @@ load_parameter_frame <- function(name) {
   file_path <- system.file("parameters", csv_name, package = "allometric")
   utils::read.csv(file_path, na.strings = "")
 }
+
+
+#' @export
+load_publication <- function(pub_id) {
+  # Ensure publication list is up-to-date, for most users this wont matter, but
+  # during model installation it will be helpful to regenerate this if changes
+  # occurred during the session
+  get_pub_list()
+  pub_list <- readRDS(system.file('extdata/pub_list.RDS', package = 'allometric'))
+  pub_list[[pub_id]]
+}
