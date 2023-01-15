105f91b4223dd90751e9afd17bf86cbdf33ad389
brycefrank <bfrank70@gmail.com>
2023-01-15 15:34:19 -0800

    feat: adding uller_2019, closes #46

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ refer to the [Installing a
 Model](https://brycefrank.com/allometric/articles/installing_a_model.html)
 vignette.
 
-Currently, `allometric` contains 433 allometric models across 21
+Currently, `allometric` contains 443 allometric models across 22
 publications. Refer to the
 [Reference](https://brycefrank.com/allometric/reference/index.html) for
 a full list of publications disaggregated by allometric model type.
@@ -92,7 +92,7 @@ unnested_models <- unnest_models(allometric_models, cols = 'family_name')
 unnested_models
 ```
 
-    #> # A tibble: 1,351 × 13
+    #> # A tibble: 1,411 × 13
     #>    id       component measure country   region    family        genus    species     model      pub_id        family_name                 covt_name pub_year
     #>    <chr>    <chr>     <chr>   <list>    <list>    <chr>         <chr>    <chr>       <list>     <chr>         <chr>                       <list>       <dbl>
     #>  1 3aff0a28 stem      volume  <chr [1]> <chr [1]> Aceraceae     Acer     <NA>        <FxdEffcM> brackett_1977 "Brackett"                  <chr [2]>     1977
@@ -105,7 +105,7 @@ unnested_models
     #>  8 fc847c75 tree      biomass <chr [1]> <chr [1]> Aquifoliaceae Ilex     canariensis <FxdEffcM> montero_2005  "Munoz"                     <chr [1]>     2005
     #>  9 1460a26f stem      biomass <chr [1]> <chr [1]> Aquifoliaceae Ilex     canariensis <FxdEffcM> montero_2005  "Montero"                   <chr [1]>     2005
     #> 10 1460a26f stem      biomass <chr [1]> <chr [1]> Aquifoliaceae Ilex     canariensis <FxdEffcM> montero_2005  "Ruiz-Peinado"              <chr [1]>     2005
-    #> # … with 1,341 more rows
+    #> # … with 1,401 more rows
 
 Now, each row represents unique data combinations for each model, which
 can be quickly filtered by most users using `dplyr::filter`. For
---
+++ inst/publications/uller_2019.R
@@ -0,0 +1,189 @@
+uller_2019 <- Publication(
+  citation = RefManageR::BibEntry(
+    bibtype = "article",
+    key = "uller_2019",
+    author = "Uller, Heitor Felippe and Zimermann Oliveira, Laio and Klitzke, Aline Renata and Roberto Eleotério, Jackson and Fantini, Alfredo Celso and Vibrans, Alexander Christian",
+    title = "Aboveground biomass quantification and tree-level prediction models for the Brazilian subtropical Atlantic Forest",
+    journal = "Southern Forests",
+    volume = 81,
+    number = 3,
+    pages = "261--271",
+    year = 2019,
+    publisher = "NISC"
+  ),
+  descriptors = list(
+    country = "BR",
+    region = "BR-SC"
+  )
+)
+
+mod_1 <- FixedEffectsModel(
+  response_unit = list(bt = units::as_units('kg')),
+  covariate_units = list(dsob = units::as_units('cm')),
+  parameters = list(
+    beta_0 = -48.4365,
+    beta_1 = 0.6467
+  ),
+  predict_fn = function(dsob) {
+    beta_0 + beta_1 * dsob^2
+  }
+)
+
+mod_2 <- FixedEffectsModel(
+  response_unit = list(bt = units::as_units('kg')),
+  covariate_units = list(dsob = units::as_units('cm')),
+  parameters = list(
+    beta_0 = -2.3702,
+    beta_1 = 2.5179,
+    bcf = 1.0542
+  ),
+  predict_fn = function(dsob) {
+    exp(log(beta_0) + beta_1 * log(dsob)) * bcf
+  }
+)
+
+mod_3 <- FixedEffectsModel(
+  response_unit = list(bt = units::as_units('kg')),
+  covariate_units = list(
+    dsob = units::as_units('cm'),
+    hst = units::as_units('m')
+  ),
+  parameters = list(
+    beta_0 = 25.9310,
+    beta_1 = 0.0258
+  ),
+  predict_fn = function(dsob, hst) {
+    beta_0 + beta_1 * (dsob^2 * hst)
+  }
+)
+
+mod_4 <- FixedEffectsModel(
+  response_unit = list(bt = units::as_units('kg')),
+  covariate_units = list(
+    dsob = units::as_units('cm'),
+    hst = units::as_units('m')
+  ),
+  parameters = list(
+    beta_0 = -3.4062,
+    beta_1 = 0.9774,
+    bcf = 1.0494
+  ),
+  predict_fn = function(dsob, hst) {
+    exp(log(beta_0) + beta_1 * log(dsob^2 * hst)) * bcf
+  }
+)
+
+mod_5 <- FixedEffectsModel(
+  response_unit = list(bt = units::as_units('kg')),
+  covariate_units = list(
+    dsob = units::as_units('cm'),
+    hst = units::as_units('m')
+  ),
+  parameters = list(
+    beta_0 = -3.1284,
+    beta_1 = 2.1077,
+    beta_2 = -0.7103,
+    bcf = 1.0468
+  ),
+  predict_fn = function(dsob, hst) {
+    exp(log(beta_0) + beta_1 * log(dsob) + beta_2 * log(hst)) * bcf
+  }
+)
+
+mod_6 <- FixedEffectsModel(
+  response_unit = list(bt = units::as_units('kg')),
+  covariate_units = list(
+    dsob = units::as_units('cm'),
+    hst = units::as_units('m')
+  ),
+  parameters = list(
+    beta_0 = -3.1501,
+    beta_1 = 2.0693,
+    beta_2 = 0.7595,
+    bcf = 1.0456
+  ),
+  predict_fn = function(dsob, hst) {
+    exp(log(beta_0) + beta_1 * log(dsob^2) + beta_2 * log(hst)) * bcf
+  }
+)
+
+mod_7 <- FixedEffectsModel(
+  response_unit = list(bt = units::as_units('kg')),
+  covariate_units = list(
+    dsob = units::as_units('cm'),
+    rsd = units::as_units('kg / m^3')
+  ),
+  parameters = list(
+    beta_0 = -9.0086,
+    beta_1 = 2.4606,
+    beta_2 = 1.0895,
+    bcf = 1.0206
+  ),
+  predict_fn = function(dsob, rsd) {
+    exp(log(beta_0) + beta_1 * log(dsob) + beta_2 * log(rsd)) * bcf
+  }
+)
+
+mod_8 <- FixedEffectsModel(
+  response_unit = list(bt = units::as_units('kg')),
+  covariate_units = list(
+    dsob = units::as_units('cm'),
+    hst = units::as_units('m'),
+    rsd = units::as_units('kg / m^3')
+  ),
+  parameters = list(
+    beta_0 = 10.2861,
+    beta_1 = 0.00005
+  ),
+  predict_fn = function(dsob, hst, rsd) {
+    beta_0 + beta_1 * (dsob^2 * hst * rsd)
+  }
+)
+
+mod_9 <- FixedEffectsModel(
+  response_unit = list(bt = units::as_units('kg')),
+  covariate_units = list(
+    dsob = units::as_units('cm'),
+    hst = units::as_units('m'),
+    rsd = units::as_units('kg / m^3')
+  ),
+  parameters = list(
+    beta_0 = -9.1954,
+    beta_1 = 0.9561,
+    bcf = 1.0209
+  ),
+  predict_fn = function(dsob, hst, rsd) {
+    exp(log(beta_0) + beta_1 * log(dsob^2 * hst * rsd)) * bcf
+  }
+)
+
+mod_10 <- FixedEffectsModel(
+  response_unit = list(bt = units::as_units('kg')),
+  covariate_units = list(
+    dsob = units::as_units('cm'),
+    hst = units::as_units('m'),
+    rsd = units::as_units('kg / m^3')
+  ),
+  parameters = list(
+    beta_0 = -8.8907,
+    beta_1 = 2.1642,
+    beta_2 = 0.5072,
+    beta_3 = 0.9999,
+    bcf = 1.0175
+  ),
+  predict_fn = function(dsob, hst, rsd) {
+    exp(log(beta_0) + beta_1 * log(dsob) + beta_2 * log(hst) + beta_3 * log(rsd)) * bcf
+  }
+)
+
+uller_2019 <- uller_2019 %>%
+  add_model(mod_1) %>%
+  add_model(mod_2) %>%
+  add_model(mod_3) %>%
+  add_model(mod_4) %>%
+  add_model(mod_5) %>%
+  add_model(mod_6) %>%
+  add_model(mod_7) %>%
+  add_model(mod_8) %>%
+  add_model(mod_9) %>%
+  add_model(mod_10)
