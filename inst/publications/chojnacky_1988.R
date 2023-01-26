4178b3b988c837efb06339a6f110c222fbb8a954
brycefrank <bfrank70@gmail.com>
2023-01-26 15:22:52 -0800

    feat: adding chojnacky_1988, closes #64

---
+++ inst/publications/chojnacky_1988.R
@@ -0,0 +1,50 @@
+chojnacky_1988 <- Publication(
+  citation = RefManageR::BibEntry(
+    bibtype = "techreport",
+    title = "Juniper, pinyon, oak, and mesquite volume equations for Arizona",
+    author = "Chojnacky, David C",
+    volume = 391,
+    year = 1988,
+    institution = "US Department of Agriculture, Forest Service, Intermountain Research Station"
+  ),
+  descriptors = list(
+    country = "US",
+    region = "US-AZ"
+  )
+)
+
+vsa_small <- FixedEffectsSet(
+  response_unit = list(
+    vsa = units::as_units('ft^3')
+  ),
+  covariate_units = list(
+    dsoc = units::as_units('in'),
+    hst = units::as_units('ft')
+  ),
+  parameter_names = c('beta_0', 'beta_1', 'beta_2'),
+  predict_fn = function(dsoc, hst) {
+    X <- dsoc^2 * hst / 1000
+    beta_0 + beta_1 * X + beta_2 * X^2
+  },
+  model_specifications = load_parameter_frame('vsa_1988_1')
+)
+
+vsa_large <- FixedEffectsSet(
+  response_unit = list(
+    vsa = units::as_units('ft^3')
+  ),
+  covariate_units = list(
+    dsoc = units::as_units('in'),
+    hst = units::as_units('ft')
+  ),
+  parameter_names = c('beta_3', 'beta_1', 'beta_4'),
+  predict_fn = function(dsoc, hst) {
+    X <- dsoc^2 * hst / 1000
+    beta_3 + beta_1 * X + beta_4 / X
+  },
+  model_specifications = load_parameter_frame('vsa_1988_2')
+)
+
+chojnacky_1988 <- chojnacky_1988 %>%
+  add_set(vsa_small) %>%
+  add_set(vsa_large)
