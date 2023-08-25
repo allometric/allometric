e9978b10e464995756e1044e94ab314f35f5d0b4
brycefrank <bfrank70@gmail.com>
2023-08-24 19:45:42 -0700

    fix: install_models no longer pulls if isntalled

--- R/install.R
+++ R/install.R
@@ -40,28 +40,20 @@ delete_models <- function() {
 #' @export
 install_models <- function(ignore_cache = FALSE, verbose = FALSE) {
   downloaded <- check_models_downloaded()
+  pkg_path <- system.file("", package = "allometric")
+  model_dir_path <- file.path(pkg_path, "models")
 
   if(!downloaded) {
-    print("Cloning allometric/models repository.")
-
-    pkg_path <- system.file("", package = "allometric")
-    model_dir_path <- file.path(pkg_path, "models")
-
     dir.create(model_dir_path)
 
+    print("Cloning allometric/models repository.")
     gert::git_clone(
       "https://github.com/allometric/models.git",
       path = model_dir_path,
-      verbose = FALSE,
-      overwrite = TRUE
+      verbose = FALSE
     )
-  } else {
-    print("Pulling allometric/models repository.")
-
-    pkg_path <- system.file("", package = "allometric")
-    model_dir_path <- file.path(pkg_path, "models")
-    gert::git_pull(repo = model_dir_path)
   }
+
 
   run_pub_list <- get_run_pubs(ignore_cache, verbose)
   update_pub_list(run_pub_list)
