7f1ba67cb06c8d6b3692c038e332a55eb3b18951
brycefrank <bfrank70@gmail.com>
2023-09-13 10:01:02 -0700

    fix: implementing semantic-release, closes #141

--- set-version.ps1
+++ set-version.ps1
@@ -1,1 +1,2 @@
 (Get-Content DESCRIPTION) -replace 'Version: .*', "Version: $args" | Set-Content DESCRIPTION
+(Get-Content package.json) -replace '  "version": "0.0.1"', '  "version": $args"' | Set-Content package.json
