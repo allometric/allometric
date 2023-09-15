c83744a3704d3f6081c6bb7752f68c1cb5b97047
brycefrank <bfrank70@gmail.com>
2023-09-15 14:01:59 -0700

    test: Adding some tests for variable naming system

--- tests/testthat/test-variable-defs.R
+++ tests/testthat/test-variable-defs.R
@@ -28,3 +28,23 @@ test_that("get_variable_def returns correct defintion with prefix and suffix", {
 
   expect_equal(matches$description, "tree biomass (plot-level increment)")
 })
+
+test_that("prepare_var_defs returns a list equal to the length of the number of measures", {
+  n_measures <- nrow(measure_defs)
+  var_defs <- prepare_var_defs(var_defs_pre, measure_defs, component_defs)
+
+  expect_equal(length(var_defs), n_measures)
+})
+
+test_that("get_measure_defs returns a non-empty dataframe", {
+  measure_defs_ <- get_measure_defs()
+
+  expect_true(is.data.frame(measure_defs_))
+  expect_true(nrow(measure_defs) > 0)
+})
+
+test_that("matched model type returns correct value", {
+  matched_type <- get_model_type("dsoh")
+
+  expect_equal("taper", matched_type)
+})
