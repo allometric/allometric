88eb8be3be1d5add517d8c51f2ab3ec9977ecf7c
brycefrank <bfrank70@gmail.com>
2023-10-13 08:25:03 -0700

    adding equivalence method for Taxon and match (%in%) method for Taxon,list and list,Taxon

--- R/Taxon.R
+++ R/Taxon.R
@@ -23,3 +23,44 @@ Taxon <- function(family = NA_character_, genus = NA_character_, species = NA_ch
   taxon <- .Taxon(family = family, genus = genus, species = species)
   taxon
 }
+
+# This function returns TRUE wherever elements are the same, including NA's,
+# and FALSE everywhere else.
+compareNA <- function(v1, v2) {
+    same <- (v1 == v2) | (is.na(v1) & is.na(v2))
+    same[is.na(same)] <- FALSE
+    return(same)
+}
+
+setMethod("==", signature(e1 = "Taxon", e2 = "Taxon"),
+  function(e1, e2) {
+    # All fields in e1 e2 are NA_character_
+    e1_vals <- c(e1@family, e1@genus, e1@species)
+    e2_vals <- c(e2@family, e2@genus, e2@species)
+
+    all(compareNA(e1_vals, e2_vals))
+})
+
+setMethod("%in%", signature(x = "Taxon", table = "list"),
+  function(x, table) {
+    for(taxon in table) {
+      if(x == taxon) {
+        return(TRUE)
+      }
+    }
+
+    return(FALSE)
+  }
+)
+
+setMethod("%in%", signature(x = "list", table = "Taxon"),
+  function(x, table) {
+    for(taxon in x) {
+      if(taxon == x) {
+        return(TRUE)
+      }
+    }
+
+    return(FALSE)
+  }
+)
